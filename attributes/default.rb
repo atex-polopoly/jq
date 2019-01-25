d7e7090e18546ef67750863a0ec885dc27573900
David Wood <dwood@atex.com>
2019-01-25 09:00:29 +0000

    Ensure required attributes are defined

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,7 @@
 default['jq']['install_dir'] = '/srv'
 default['jq']['name'] = 'jq'
 default['jq']['version'] = '1.5'
+default['mirror']['host'] = 'mirror.cloud.atex.com:8081'
+default['mirror']['path'] = '/'
+
 
